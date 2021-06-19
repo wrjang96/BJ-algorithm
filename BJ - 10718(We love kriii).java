29117a4a8536114ab95b46570ef8349416188d35
wrjang96 <66419215+wrjang96@users.noreply.github.com>
2021-06-19 23:56:17 +0900

    21/06/19

---
+++ BJ - 10718(We love kriii).java
@@ -0,0 +1,16 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) 
+	{
+		for(int i=0 ; i<2 ; i++){
+		System.out.println("강한친구 대한육군");
+		}
+	}
+}
