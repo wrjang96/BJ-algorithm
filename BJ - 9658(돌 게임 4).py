b2a7714fcbd203160f1bc2972f41551048460496
wrjang96 <66419215+wrjang96@users.noreply.github.com>
2021-07-21 10:18:54 +0900

    21/07/21

---
+++ BJ - 9658(돌 게임 4).py
@@ -0,0 +1,5 @@
+N = int(input())
+if N%7 == 3 or N%7 == 1:
+    print("CY")
+else:
+    print("SK")
---
+++ BJ - 9661(돌 게임 7).py
@@ -0,0 +1,5 @@
+N = int(input())
+if (N%5) == 2 or (N%5) == 0:
+    print("CY")
+else:
+    print("SK")
