15d0eecb7f9c53dbdebbf6526323acecb98f09b9
wrjang96 <wrjang@naver.com>
2021-11-19 04:38:32 +0900

    21/11/19

---
+++ BJ - 2252(줄 세우기).py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+N, M = map(int, input().split())
+indegree = [0] * N
+dp = [0] * N
+graph = [[] for i in range(N)]
+
+for i in range(M):
+    start, end = map(int, input().split())
+    start -= 1
+    end -= 1
+    graph[start].append(end)
+    indegree[end] +=1
+
+stack = deque()
+
+for i in range(N):
+    if indegree[i] == 0:
+        stack.append(i)
+        dp[i] += 1
+
+while stack:
+    temp = stack.popleft()
+    print(temp + 1, end =' ')
+    for next_temp in graph[temp]:
+        indegree[next_temp] -= 1
+        dp[next_temp] = max(dp[temp] + 1, dp[next_temp])
+        if indegree[next_temp] ==0:
+            stack.append(next_temp)
+
+# print(*dp)
