4df4aad30ba9aab4fdef25423d8671356ac949ed
wrjang96 <wrjang@naver.com>
2022-03-06 17:52:57 +0900

    22/03/06

---
+++ BJ - 1965(상자 넣기).py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+dp = [1] * len(arr)
+
+for i in range(1, N):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+# print(dp)
+print(max(dp))
