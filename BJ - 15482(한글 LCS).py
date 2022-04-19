57d2a93dacda6567d2e5eb7671e7221a7e486b6a
wrjang96 <wrjang@naver.com>
2022-04-20 00:42:02 +0900

    22/04/20

---
+++ BJ - 15482(한글 LCS).py
@@ -0,0 +1,13 @@
+word1 = input()
+word2 = input()
+l1, l2 = len(word1), len(word2)
+cache = [0] * l2
+
+for i in range(l1):
+    cnt = 0
+    for j in range(l2):
+        if cnt < cache[j]:
+            cnt = cache[j]
+        elif word1[i] == word2[j]:
+            cache[j] = cnt + 1
+print(max(cache))
